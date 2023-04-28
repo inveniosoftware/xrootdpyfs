0a889860b4c5460132eb11b9484ebe91f298d1c3
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2023-04-28 13:35:06 +0200

    fs: remove bytes from response added by memory allocation issue

--- tests/test_opener.py
+++ tests/test_opener.py
@@ -15,8 +15,7 @@ from fs.opener import open_fs
 def test_open_fs_create(tmppath):
     """Test open with create."""
     rooturl = mkurl(tmppath)
-    print(rooturl)
-    fs = open_fs(rooturl + "/non-existing")
+    fs = open_fs(f"{rooturl}/non-existing")
     assert fs.listdir("./")
     assert not fs.exists("/non-existing")
     fs = open_fs(rooturl + "/non-existing", create=True)
